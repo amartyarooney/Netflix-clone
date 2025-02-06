fd5016368baedcd30119c4e37ae10053e8a63060
Amartya <amartyarooney010@gmail.com>
2025-02-06 15:53:00 -0500

    search controller

---
+++ backend/controllers/search.controller.js
@@ -0,0 +1,82 @@
+import { response } from "express";
+import { User } from "../models/user.model.js";
+import { fetchFromTMDB } from "../services/tmdb.services.js";
+
+export async function searchPerson(req,res){
+    const {query} = req.params;
+    try {
+        
+        console.log(query);
+        const response= await fetchFromTMDB(`https://api.themoviedb.org/3/search/person?query=${query}&include_adult=false&language=en-US&page=1`);
+        console.log(response);
+        if(response.results.length === 0){
+            return res.status(404).send(null);
+        }
+        await User.findByIdAndUpdate(req.user._id,{
+        $push:{
+            searchHistory:{
+                id: response.results[0].id,
+                image: response.results[0].profile.path,
+                title:response.results[0].name,
+                searchType:"person",
+                createdAt: new Date(),
+            },
+        },
+    })
+        res.status(200).json({success:true, content:response.results});
+    } catch (error) {
+        res.status(404).json({success:false, message: error});
+    }
+    
+
+}
+
+export async function searchMovie(req,res){
+    const {query} = req.params;
+    try {
+        const response = await fetchFromTMDB(`https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`);
+        if(response.results.length === 0){
+            res.status(404).send(null);
+        }
+        await User.findByIdAndUpdate(req.user._id,{
+            $push :{
+                searchHistory:{
+                id : response.results[0].id,
+                image:response.results[0].poster_path,
+                title: response.results[0].title,
+                searchType: "movie",
+                createdAt: new Date(),
+            },
+        },
+        })
+        res.status(200).json({success:true, content:response.results});
+     } catch (error) {
+        res.status(404).json({success:false, message: error})
+    }
+
+}
+
+export async function searchTv(req,res){
+    const {query} = req.params;
+    try {
+        const response = await fetchFromTMDB(`https://api.themoviedb.org/3/search/tv?query=${query}&include_adult=false&language=en-US&page=1`);
+        if(response.results.length === 0){
+            res.status(404).send(null);
+        }
+        await User.findByIdAndUpdate(req.user._id,{
+            searchHistory:{
+            $push :{
+                id : response.results[0].id,
+                image:response.results[0].poster_path,
+                title: response.results[0].name,
+                searchType: "tv",
+                createdAt: new Date(),
+            },
+            },
+        })
+        res.status(200).json({success:true, content:response.results});
+        } catch (error) {
+        res.status(404).json({success:false, message: error})
+    }
+
+}
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
 import tvRoutes from "./routes/tv.route.js";
+import searchRoutes from "./routes/search.route.js";
+
 import { protectRoute } from "./middlewares/protectRoute.js";
 import dotenv from "dotenv"
 import { ENV_VARS } from "./config/envVars.js";
@@ -20,6 +22,7 @@ app.use(cookieParser());
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie",protectRoute, movieRoutes);
 app.use("/api/v1/tv",protectRoute, tvRoutes);
+app.use("/api/v1/search", protectRoute, searchRoutes);
 app.use(express.urlencoded({ extended: true }));
 console.log("does it come here?");
 app.listen(PORT, () => {
