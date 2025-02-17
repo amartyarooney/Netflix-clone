980de0af2435e917bf7312f42b7c66df2e5f8769
Amartya <amartyarooney010@gmail.com>
2025-02-17 11:40:44 -0500

    footer section added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,18 @@ import { Route, Routes } from "react-router-dom"
 import SignupPage from "./pages/SignupPage"
 import HomePage from "./pages/home/HomePage"
 import LoginPage from "./pages/LoginPage"
+import Footer from "./components/Footer"
 
 function App() {
   return (
+    <>
     <Routes>
       <Route path='/' element={<HomePage />} />
       <Route path='/signup' element={<SignupPage />} />
       <Route path='/login' element={<LoginPage />} />
-
     </Routes>
+    <Footer />
+    </>
   )
 }
 
--- frontend/src/pages/home/AuthScreen.jsx
+++ frontend/src/pages/home/AuthScreen.jsx
@@ -7,7 +7,7 @@ const AuthScreen = () => {
   return <div className='hero-bg relative'>
       {/* navbar */}
       <header className='max-w-6xl mx-auto flex items-center justify-between p-4 pb-10'>
-        <img src='/netflix.logo.png' alt='netflix logo' className='w-32 md:w-52' />
+        <img src='/netflix-logo.png' alt='netflix logo' className='w-32 md:w-52' />
         <Link to={"/login"} className='text-white bg-red-600 py-1 px-2 rounded'>
         Sign In
         </Link>
@@ -92,6 +92,54 @@ const AuthScreen = () => {
 
         </div>
        </div>
+       <div className='h-2 w-full bg-[#232323]' aria-hidden='true'/>{/* separator*/}
+       {/* 1st section*/}
+
+       <div className='py-10 bg-black text-white'>
+            <div className='flex max-w-6xl mx-auto items-center justify-center md:flex-row flex-col px-4 md:px-2'>
+                {/* left*/}
+                <div className=' flex-1 text-center md:text-left '>
+                    <h2 className='text-4xl md:text-5xl font-extrabold mb-4'> Watch Evrywhere</h2>
+                    <p className='text-lg md:text-xl'>
+                        Stream Unlimited Movies and Tv Shows on your Laptop, Phone, Tablet, and Tv.
+                    </p>
+                </div>
+                {/* right side */}
+                <div className='flex-1 relative overflow-hidden'>
+                    <img src="/device-pile.png" alt="device image" className='mt-4 z-20 relative' />
+                    <video className='absolute top-2 left-1/2 -translate-x-1/2  h-4/6 z-10 max-w-[63%]'
+                    playsInline
+                    autoPlay={true}
+                    muted
+                    loop
+                    >
+                        <source  src='/video-devices.m4v' type='video/mp4'/>
+                    </video>
+                </div>
+
+        </div>
+        
+       </div>
+       <div className='h-2 w-full bg-[#232323]' aria-hidden='true'/>{/* separator*/}
+       {/* 4th section */}
+       <div className=' bg-black py-10 text-white'>
+
+       
+       <div className='flex max-w-6xl mx-auto items-center justify-center md:flex-row flex-col-reverse px-4 md:px-2'>
+            {/* left ride */}
+            <div className='flex-1 relative'>
+                <img src='/kids.png' alt='kids image' className='mt-4' ></img>
+            </div>
+            {/* right ride */}
+            <div className='flex-1 text-center md:text-left'>
+            <h2 className='text-4xl font-extrabold mb-4 md:text-5xl'> Create profile for kids</h2>
+            <p1 className='text-lg md:text-xl'>Send kids adventures with their favorite characters in a space made just for them-free with your membership</p1>
+            </div>
+        </div>
+        </div>
+
+        {/*  */}
+       
     </div>
 } 
 
