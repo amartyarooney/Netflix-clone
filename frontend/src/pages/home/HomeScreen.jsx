e3f5d2e005d3c8b0cc1a067e761872410b6ca48d
Amartya <amartyarooney010@gmail.com>
2025-03-03 18:02:52 -0500

    shimmer effect added

--- frontend/src/pages/home/HomeScreen.jsx
+++ frontend/src/pages/home/HomeScreen.jsx
@@ -7,6 +7,14 @@ const HomeScreen = () => {
     const {trendingContent} = useGetTrendingContent();
     console.log("trendingContent", trendingContent); 
     // Add a loaded spinner 
+    if(!trendingContent) return (
+        <div className='h-screen text-white relative'>
+            <Navbar />
+            <div 
+            className='absolute top-0 left-0 w-full h-full bg-black/70 flex items-center justify-center -z-10 shimmer'
+            />
+        </div>
+    )
 
     return  (
         <>
