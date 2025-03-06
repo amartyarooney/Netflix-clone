f59b86238bb91a219ddd49101538c1563e2b8efa
Amartya <amartyarooney010@gmail.com>
2025-03-05 23:42:44 -0500

    commit

--- frontend/src/components/MovieSlider.jsx
+++ frontend/src/components/MovieSlider.jsx
@@ -1,12 +1,16 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useContentStore } from "../../store/content";
 import { Link } from "react-router-dom";
 import { SMALL_IMG_BASE_URL } from "../utils/constants";
 import axios from "axios";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 const MovieSlider = ({category}) => {
     const {contentType} = useContentStore();
     const [content, setContent] = useState([]);
+    const [showArrows, setShowArrows] = useState(false);
+    const sliderRef = useRef(null);
+
     const formattedCategoryName = category.replaceAll("_", " ")[0].toUpperCase()+ category.replaceAll("_", " ").slice(1);
     const formattedContentType = contentType === "movie" ? "Movies" : "TV shows";
     useEffect(()=> {
@@ -15,10 +19,28 @@ const MovieSlider = ({category}) => {
             setContent(res.data.content);
         }
         getContent();
-    },[contentType, category])
-  return (  <div className=' bg-black text-white relative px-5 md:px-20'>
+    },[contentType, category]);
+
+    const scrollLeft = () => {
+        if(sliderRef.current) {
+            sliderRef.current.scrollBy({left: -sliderRef.current.offsetWidth, behaviour: 'smooth'})
+        }
+    }
+
+    const scrollRight = () => {
+        if(sliderRef.current) {
+            sliderRef.current.scrollBy({right: sliderRef.current.offsetWidth, behaviour: 'smooth'})
+        }
+    }
+
+
+
+
+  return (  <div className=' bg-black text-white relative px-5 md:px-20' onMouseEnter={() => setShowArrows(true)}
+                onMouseLeave={() => setShowArrows(false)} 
+                >
       <h2 className='mb-4 text-2xl font-bold'>{formattedCategoryName} {formattedContentType}</h2>
-      <div className='flex space-x-4 overflow-x-scroll'>
+      <div className='flex space-x-4 overflow-x-scroll' ref={sliderRef}>
         {content.map((item) => (
             <Link to={`/watch/${item.id}`} className='min-w-[250px] relative group' key={item.id}>
                 <div className='rounded-lg overflow-hidden'>
@@ -31,6 +53,21 @@ const MovieSlider = ({category}) => {
             </Link>
         ))}
       </div>
+      {showArrows && (
+        <>
+            <button className='absolute top-1/2 -translate-y-1/2 left-5 md:left-24 flex items-center justify-center
+            size-12 rounded-full bg-black bg-opacity-50 hover:bg-opacity-75 text-white z-10' onClick={scrollLeft}>
+                <ChevronLeft size={24}/>
+                
+            </button>
+            <button className='absolute top-1/2 -translate-y-1/2 right-5 md:right-24 flex items-center justify-center
+            size-12 rounded-full bg-black bg-opacity-50 hover:bg-opacity-75 text-white z-10' onClick={scrollRight}>
+                <ChevronRight size={24}/>
+                
+            </button>
+
+        </>
+      )}
     </div>)
   
 };
