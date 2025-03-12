e654bacab69e4870bb5e40d45a996d1e4fd2ca4a
Amartya <amartyarooney010@gmail.com>
2025-03-11 23:28:37 -0400

    big image loading shimmer effect

--- frontend/src/pages/home/HomeScreen.jsx
+++ frontend/src/pages/home/HomeScreen.jsx
@@ -5,12 +5,15 @@ import useGetTrendingContent from "../../hooks/useGetTrendingContents";
 import { MOVIE_CATEGORIES, ORIGINAL_IMG_BASE_URL, TV_CATEGORIES } from "../../utils/constants";
 import { useContentStore } from "../../../store/content";
 import MovieSlider from "../../components/MovieSlider";
+import { useState } from "react";
 const HomeScreen = () => {
     const {trendingContent} = useGetTrendingContent();
     const  {contentType} = useContentStore();
+    const [imgLoading, setImgLoading] = useState(true);
     if(!trendingContent) return (
         <div className='h-screen text-white relative'>
             <Navbar />
+            
             <div 
             className='absolute top-0 left-0 w-full h-full bg-black/70 flex items-center justify-center -z-10 shimmer'
             />
@@ -21,10 +24,16 @@ const HomeScreen = () => {
         <>
             <div className='relative h-screen text-white'>
                 <Navbar />
+                {imgLoading &&  (<div 
+            className='absolute top-0 left-0 w-full h-full bg-black/70 flex items-center justify-center -z-10 shimmer'
+            />)}
                 <img 
                     src={ORIGINAL_IMG_BASE_URL + trendingContent?.backdrop_path}
                     alt='hero img'
                     className='absolute top-0 left-0 w-full h-full object-cover -z-50'
+                    onLoad={() => {
+                        setImgLoading(false);
+                    }}
                     />
                 <div className='absolute top-0 left-0 w-full h-full bg-black/50 -z-50' aria-hidden= 'true' >
                     <div className=' absolute top-0 left-0 w-full h-full flex flex-col 
