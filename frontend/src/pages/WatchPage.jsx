3e43e34553774894920ede5e6a1f287e7d6fdf9c
Amartya <amartyarooney010@gmail.com>
2025-03-17 17:48:54 -0400

    buttons added

--- frontend/src/pages/WatchPage.jsx
+++ frontend/src/pages/WatchPage.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 import { useContentStore } from "../../store/content";
 import axios from "axios";
+import Navbar from "../components/Navbar";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 const WatchPage = () => {
 
@@ -11,6 +13,8 @@ const WatchPage = () => {
     const [loading, setLoading] = useState(true);
     const [content, setContent] = useState({});
     const [similarContent, setSimilarContent] = useState([]);
+    // const [movieDetails, setMovieDetails] = useState([]);
+
 
     const {contentType} = useContentStore();
 
@@ -40,15 +44,59 @@ const WatchPage = () => {
                 setSimilarContent(res.data.similarMovies.results);
             } catch (error) {
                 if(error.message.includes(404)){
-                    setSimilarContent([]);
+                    setSimilarContent(null);
                 }
             }
         }
         getSimilarContent();
-    }, []);
+    }, [contentType, id]);
+
+    useEffect(() => {
+        const getContentDetails = async () =>{
+            try {
+                const res = await axios.get(`/api/v1/${contentType}/${id}/details`);
+                setContent(res.data.details);
+            } catch (error) {
+                if(error.message.includes(404)){
+                    setContent([]);
+                }
+            }
+        }
+        getContentDetails();
+    }, [contentType, id]);
          
-    console.log("similr: ", similarContent);
-    return  <div> HI </div>
+    const handleNext = () => {
+        if(currentTrailerIdx >= 0)
+            setCurrentTrailerIdx(currentTrailerIdx + 1);
+    }
+    const handlePrev = () => {
+        if(currentTrailerIdx <= trailers.length-1)
+            setCurrentTrailerIdx(currentTrailerIdx - 1);
+    }
+
+    return  <div className='bg-black min-h-screen text-white'>
+                <Navbar/>
+                {trailers.length > 0 && (
+                    <div className='flex justify-between items-center mb-4 cursor:pointer '>
+                        <button className={` bg-gray-500/70 hover:bg-gray-500 text-white py-2 px-4 rounded cursor:pointer
+                            ${currentTrailerIdx === 0 ? " opacity-50 cursor-not-allowed " : ""}}`}
+                            disabled={currentTrailerIdx === 0}
+                            onClick={handlePrev}
+                            >
+                            <ChevronLeft  size={24}  />
+                        </button>
+
+                        <button className={` bg-gray-500/70 hover:bg-gray-500 text-white py-2 px-4 rounded 
+                            ${currentTrailerIdx === trailers.length-1 ? " opacity-50 cursor-not-allowed " : ""}}`}
+                            disabled={currentTrailerIdx === trailers.length-1} 
+
+                            onClick={handleNext}>
+                            <ChevronRight  size={24} />
+                            {console.log("gvvhjjhb: ",trailers.length , currentTrailerIdx )}
+                        </button>
+                    </div>
+                )}
+            </div>
   
 }
 
